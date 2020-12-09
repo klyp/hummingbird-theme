df596efa6fc5fc1053ad190b5130d1bdd869c93f
atulkumarsingh12 <atulkumarsingh@cedcoss.com>
2020-12-09 15:16:37 +0530

    added tile frontend component

--- templates/components/tile.php
+++ templates/components/tile.php
@@ -9,7 +9,7 @@
 ?>
 
 <?php if ($enableComponent) : ?>
-    <section id="<?php echo $componentId; ?>" class="hb-counter hb-bg-light text-center <?php echo $componentClass; ?>">
+    <section id="<?php echo $componentId; ?>" class="hb-tile hb-bg-light text-center <?php echo $componentClass; ?>">
         <div class="hb-container">
             <div class="hb-row">
                 <?php foreach ($tileData as $data) { ?>
@@ -17,11 +17,15 @@
                     <?php if (! empty($data['image'])) { ?>
                         <?php $style = 'style="background-image: url(' . $data['image'] . ');"' ?>
                     <?php } ?>
-                    <div class="col-4" <?php echo $style; ?>>
-                        <a href="<?php echo $data['link']['url']; ?>"
-                            <?php echo ($data['link']['target']) ? 'target="_blank"' : ''; ?>>
-                            <?php echo $data['link']['title']; ?>
-                        </a>
+                    <div class="col-md-4 hb-tile__column">
+                        <div class="hb-tile__column-bg" <?php echo $style; ?>>
+                            <h4>
+                                <a href="<?php echo $data['link']['url']; ?>"
+                                    <?php echo ($data['link']['target']) ? 'target="_blank"' : ''; ?>>
+                                    <?php echo $data['link']['title']; ?>
+                                </a>
+                            </h4>
+                        </div>
                     </div>
                 <?php } ?>
             </div>
