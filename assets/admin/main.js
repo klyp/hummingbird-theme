5e619db331f3fc1580509dbf5e8fbd25ada812d0
Nyco Agung <nyco.agung@klyp.co>
2021-05-04 13:43:55 +1000

    fixes on code review

--- assets/admin/main.js
+++ assets/admin/main.js
@@ -40,31 +40,6 @@
             var target = $(this).data('target');
 
             $('#klyp-import-page-id').val(target);
-
-            // if (target != '') {
-            //     $.ajax({
-            //         type: 'post',
-            //         url: adminUrl,
-            //         data: {
-            //             action: 'klyp_export_components',
-            //             nonce: nonce,
-            //             id: target
-            //         },
-            //         success: function(response) {
-            //             if (response.data.result != '') {
-            //                 const blob = new Blob([response.data.result], {type: "octet/stream"}),
-            //                       url = window.URL.createObjectURL(blob);
-
-            //                 window.URL.revokeObjectURL(url);
-            //                 console.log(response.data.result);
-            //             } else {
-            //                 alert(response.data.message);
-            //             }
-            //         }
-            //     });
-            // } else {
-            //     alert('Page error - components not exported');
-            // }
         });
     });
 })(jQuery);
