5c73f7de7c522f0920cbf5ad8a4f39723f334557
Nyco Agung <nyco.agung@klyp.co>
2021-05-10 14:20:33 +1000

    fixed index require

--- includes/index.php
+++ includes/index.php
@@ -3,7 +3,7 @@
 /**
  * Add global component functions.
  */
-require get_theme_file_path('includes/global-components.php');
+require get_template_directory() . '/includes/global-components.php';
 
 /**
  * Adds ACF.
@@ -50,27 +50,27 @@ require get_template_directory() . '/includes/image-optimize.php';
  * Page Speed Optimization.
  */
 if (! is_user_logged_in()) {
-    require get_theme_file_path('includes/page-speed.php');
+    require get_template_directory() . '/includes/page-speed.php';
 }
 
 /**
  * Adds Custom post type: Global Components.
  */
-require get_theme_file_path('includes/post-types/global-components.php');
+require get_template_directory() . '/includes/post-types/global-components.php';
 
 /**
  * Adds extra global components post type acf field for site.
  */
-require get_theme_file_path('includes/global-components-settings-acf.php');
+require get_template_directory() . '/includes/global-components-settings-acf.php';
 
 /**
  * If admin, then add all these.
  */
 if (is_admin()) {
-    require get_theme_file_path('includes/admin.php');
+    require get_template_directory() . '/includes/admin.php';
 }
 
 /**
  * Logging.
  */
-require get_theme_file_path('includes/log/index.php');
+require get_template_directory() . '/includes/log/index.php';
